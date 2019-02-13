93ddf9e1378c28b8c903f9dca94c9eddabeef416
Zak <zakariyya.s.uddin@gmail.com>
2019-02-13 12:38:55 +0000

    Added templating

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const hbs = require('hbs');
 const port = process.env.PORT || 3000;
 
 let app = express();
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
